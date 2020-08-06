943e93d0a41aeea213d91623df26e1dd57871247
cosil2b2 <53854070+cosil2b2@users.noreply.github.com>
2020-08-06 14:46:40 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//body parser가 client 에서 오는 정보를
+//서버에서 분석해서 가져올 수 있게 함
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 //mongoose라는 모듈 쓰기
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://giyoun:gs0528123!@boilerplate.r4moe.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!~~ 안녕하세요 ^^");
+  res.send("Hello World!~~ 안녕하세요 ~ ");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  //mongoDB 메소드
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    // status(200)은 성공했다는 것
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
